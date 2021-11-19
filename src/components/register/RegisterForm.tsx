685dcf3ca92ccdd02caa95142004a1ff438e7fe2
MRmlik12 <mrmlik12sup@gmail.com>
2021-11-19 20:00:55 +0100

    Implement networking and validating with formik

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -1,16 +1,113 @@
 import React, { ReactElement } from "react";
-import { Button, Container, TextField } from "@mui/material";
+import { Alert, Button, Container, Snackbar, TextField } from "@mui/material";
 import { SxProps } from "@mui/system";
 import { Theme } from "@mui/material/styles";
+import { useFormik } from "formik";
+import { sendSchoolForm } from "../../api/api";
+import { useNavigate } from "react-router-dom";
 
 const inputStyles: SxProps<Theme> = {
   marginBottom: "20px",
 };
 
+type ErrorModel = {
+  error: boolean;
+  message: string;
+};
+
 const RegisterForm = (): ReactElement => {
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  const [emailError, setEmailError] = React.useState<ErrorModel>({
+    error: false,
+    message: "",
+  });
+  const [schoolNameError, setSchoolNameError] = React.useState<ErrorModel>({
+    error: false,
+    message: "",
+  });
+  const [schoolWebsiteUrlError, setSchoolWebsiteUrlError] =
+    React.useState<ErrorModel>({
+      error: false,
+      message: "",
+    });
+  const [schoolTimetableUrlError, setSchoolTimetableUrlError] =
+    React.useState<ErrorModel>({
+      error: false,
+      message: "",
+    });
+
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      schoolName: "",
+      schoolWebsiteUrl: "",
+      schoolTimetableUrl: "",
+    },
+    validate: (values) => {
+      if (values.email.trim() === "") {
+        setEmailError({
+          error: true,
+          message: "This field is empty",
+        });
+      }
+      if (values.schoolName.trim() === "") {
+        setSchoolNameError({
+          error: true,
+          message: "This field is empty",
+        });
+      }
+      if (values.schoolTimetableUrl.trim() === "") {
+        setSchoolWebsiteUrlError({
+          error: true,
+          message: "This field is empty",
+        });
+      }
+      if (values.schoolWebsiteUrl.trim() === "") {
+        setSchoolTimetableUrlError({
+          error: true,
+          message: "This field is empty",
+        });
+      }
+    },
+    onSubmit: async (values) => {
+      const statusCode = await sendSchoolForm(
+        values.email,
+        values.schoolName,
+        values.schoolWebsiteUrl,
+        values.schoolTimetableUrl
+      );
+
+      if (statusCode === 200) {
+        navigate("/complete");
+        return;
+      }
+
+      showSnackbar(`Status code: ${statusCode.toString()}`);
+    },
+  });
+
+  const showSnackbar = (message: string) => {
+    const handleClose = () => setOpenSnackbar(false);
+
+    return (
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    );
+  };
+
   return (
     <Container
       component="form"
+      onSubmit={formik.handleSubmit}
       autoComplete="off"
       sx={{
         display: "flex",
@@ -18,11 +115,48 @@ const RegisterForm = (): ReactElement => {
         padding: "20px",
       }}
     >
-      <TextField sx={inputStyles} label="Your e-mail" variant="outlined" />
-      <TextField sx={inputStyles} label="School name" variant="outlined" />
-      <TextField sx={inputStyles} label="School website" variant="outlined" />
-      <TextField sx={inputStyles} label="School timetable" variant="outlined" />
-      <Button style={{ marginTop: "30px" }} variant="outlined">
+      <TextField
+        id="email"
+        type="email"
+        sx={inputStyles}
+        error={emailError.error}
+        helperText={emailError.message}
+        label="Your e-mail"
+        variant="outlined"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <TextField
+        id="schoolName"
+        sx={inputStyles}
+        error={schoolNameError.error}
+        helperText={schoolNameError.message}
+        label="School name"
+        variant="outlined"
+        onChange={formik.handleChange}
+        value={formik.values.schoolName}
+      />
+      <TextField
+        id="schoolWebsiteUrl"
+        sx={inputStyles}
+        error={schoolWebsiteUrlError.error}
+        helperText={schoolWebsiteUrlError.message}
+        label="School website"
+        variant="outlined"
+        onChange={formik.handleChange}
+        value={formik.values.schoolWebsiteUrl}
+      />
+      <TextField
+        id="schoolTimetableUrl"
+        sx={inputStyles}
+        error={schoolTimetableUrlError.error}
+        helperText={schoolTimetableUrlError.message}
+        label="School timetable"
+        variant="outlined"
+        onChange={formik.handleChange}
+        value={formik.values.schoolTimetableUrl}
+      />
+      <Button style={{ marginTop: "30px" }} variant="outlined" type="submit">
         Register
       </Button>
     </Container>
