f8a252ba4809814d5a5ac131cd313a110eb02e7f
MRmlik12 <mrmlik12sup@gmail.com>
2022-01-15 16:39:23 +0100

    Add lint command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,29 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier'
-  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
-    'no-use-before-define': 'off',
-    'import/no-duplicates': 'off'
-  }
-}
+    "no-console": "error",
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "no-use-before-define": "off",
+    "import/no-duplicates": "off",
+  },
+};
