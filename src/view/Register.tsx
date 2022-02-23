881da729aa4241bafdaea15e8f1b7fe71e743335
MRmlik12 <mrmlik12sup@gmail.com>
2022-02-23 18:54:27 +0100

    Change page titles

--- src/view/Register.tsx
+++ src/view/Register.tsx
@@ -1,10 +1,17 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import RegisterForm from "../components/register/RegisterForm";
 import { Container } from "@mui/material";
 import RegisterFooter from "../components/register/RegisterFooter";
 import RegisterTitle from "../components/register/RegisterTitle";
+import { useTranslation } from "react-i18next";
 
 const Register = (): ReactElement => {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    document.title = t("registerYourSchoolTitle");
+  }, []);
+
   return (
     <Container>
       <RegisterTitle />
