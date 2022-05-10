dfc2cae89e79e07359d15a68beb4b68527336ed5
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-05-10 21:36:18 +0200

    Define APP_URL env in webpack (#93)

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,7 @@ module.exports = {
   webpack: {
     plugins: [
       new webpack.DefinePlugin({
-        APP_URL: JSON.stringify("https://vulder.xyz"),
+        APP_URL: JSON.stringify(process.env.APP_URL),
         "process.env.API_URL": JSON.stringify(process.env.API_URL),
       }),
     ],
