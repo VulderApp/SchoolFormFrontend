9726d744bed4c94267313613385b67ac087596c9
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-06-04 21:42:52 +0200

    Add language selection (#107)

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,57 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Register from "./view/Register";
 import Complete from "./view/Complete";
 import { Container, Card, CardContent } from "@mui/material";
-import { RecoilRoot } from "recoil";
 import { SnackbarError } from "./components/root/SnackbarError";
 import Footer from "./components/root/Footer";
+import i18n from "./i18n";
+import { LANGUAGE_KEY } from "./contants";
+import { language } from "./states";
+import { useRecoilState } from "recoil";
+import { getLanguageShortCode } from "./components/utils/languageUtil";
+import LanguageSelection from "./components/root/LanguageSelection";
 
 const App = (): ReactElement => {
+  const [currentLanguage, setCurrentLanguage] = useRecoilState(language);
+
+  useEffect(() => {
+    if (currentLanguage === null) {
+      localStorage.setItem(LANGUAGE_KEY, getLanguageShortCode());
+      setCurrentLanguage(getLanguageShortCode());
+    }
+
+    i18n
+      .changeLanguage(currentLanguage)
+      .then((_) => localStorage.setItem(LANGUAGE_KEY, currentLanguage));
+  }, [currentLanguage]);
+
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Container
-          sx={{
-            display: "flex",
-            height: "100%",
-            width: "auto",
-            minHeight: "90vh",
-            justifyContent: "center",
-            alignItems: "center",
-            alignSelf: "center",
-          }}
-        >
-          <Card>
-            <CardContent>
-              <Routes>
-                <Route path="/" element={<Register />} />
-                <Route path="/complete" element={<Complete />} />
-              </Routes>
-            </CardContent>
-          </Card>
-        </Container>
-        <SnackbarError />
-        <Footer />
-      </BrowserRouter>
-    </RecoilRoot>
+    <BrowserRouter>
+      <LanguageSelection />
+      <Container
+        sx={{
+          display: "flex",
+          height: "100%",
+          width: "auto",
+          minHeight: "90vh",
+          justifyContent: "center",
+          alignItems: "center",
+          alignSelf: "center",
+        }}
+      >
+        <Card>
+          <CardContent>
+            <Routes>
+              <Route path="/" element={<Register />} />
+              <Route path="/complete" element={<Complete />} />
+            </Routes>
+          </CardContent>
+        </Card>
+      </Container>
+      <SnackbarError />
+      <Footer />
+    </BrowserRouter>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
