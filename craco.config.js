225092721d39942c75ea701bb4873db71709d306
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-06-04 15:46:43 +0200

    Add version footer (#106)

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,7 @@ module.exports = {
   webpack: {
     plugins: [
       new webpack.DefinePlugin({
+        APP_VERSION: JSON.stringify(require("./package.json").version),
         APP_URL: JSON.stringify(process.env.APP_URL),
         "process.env.API_URL": JSON.stringify(process.env.API_URL),
       }),
